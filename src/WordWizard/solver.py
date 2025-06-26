3159de880e3794ef079e8e7183e7080152be562f
Mauricio de Barros <mcdebarros@ucalgary.ca>
2025-06-26 08:38:52 -0600

    Restructuring

---
+++ src/WordWizard/solver.py
@@ -0,0 +1,33 @@
+def getDict():
+
+    pass
+
+def getGuess():
+
+    pass
+
+def splitWord(word):
+
+    pass
+
+def checkGuess(guess):
+
+    pass
+
+def eliminateWith(guess,oDict,score):
+
+    pass
+
+def eliminateWithout(guess,nDict,score):
+
+    pass
+
+def EliminateExact (guess,nDict,score):
+
+    pass
+
+def letterInstances(nDict):
+
+    pass
+
+def createGuess(counts,nDict)
